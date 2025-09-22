0c0d10b6598633a713f8fb40b52a7c9d0cbe6387
Riuklip <143903240+Riuklip@users.noreply.github.com>
2025-09-21 21:55:38 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ module.exports={// se exporta a la app.js
 
     },
     mysql:{
-        host: "sql.freedb.tech",
-        User :"freedb_EliCarlos",
-        password:`dT7znSD2s?Qsp&a`,
-        database:`freedb_DatatestEli`
+        host: "sql5.freesqldatabase.com",
+        User :"sql5799585",
+        password:`ALa1wA8wnA`,
+        database:`sql5799585`
     },
     jwt:{
         secret:"notasecreta"
